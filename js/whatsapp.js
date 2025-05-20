3adcc12c744b257d03574dfa504dd87ded99750f
Ignacio Belitzky <ignabelitzky@gmail.com>
2025-05-20 13:09:03 -0300

    Hotfix - Se modificaron datos de contacto

--- js/whatsapp.js
+++ js/whatsapp.js
@@ -11,10 +11,10 @@ document.addEventListener("DOMContentLoaded", function () {
         let textoBoton = "";
 
         if (opcion === "consultorio") {
-            link = "https://wa.me/5493512741854?text=Hola%20tengo%20una%20consulta%20para%20la%20veterinaria";
+            link = "https://wa.me/+5493513142528?text=Hola%20tengo%20una%20consulta%20para%20la%20veterinaria";
             textoBoton = "Contactar Consultorio Veterinario";
         } else if (opcion === "petshop") {
-            link = "https://wa.me/5493516784060?text=Hola%20tengo%20una%20consulta%20para%20el%20pet-shop";
+            link = "https://wa.me/+5493516784060?text=Hola%20tengo%20una%20consulta%20para%20el%20pet-shop";
             textoBoton = "Contactar al Pet-Shop";
         } else {
             botonDiv.style.display = "none";
